2a1ffccbae8819e6f128269c768384eb15b9cbac
Sound Fractures <muziiix.sound@gmail.com>
2020-06-07 18:18:30 +0200

    adding depend..

--- lib/Screens/Core/features/Analysis/analysis.dart
+++ lib/Screens/Core/features/Analysis/analysis.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Analysis extends StatefulWidget {
   @override
